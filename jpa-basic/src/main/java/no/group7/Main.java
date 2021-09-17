9773a7b532a6cba59dbfc6ffcfdd55062a2f8e92
AHelplessStudent <Johan.bakoy99@gmail.com>
2021-09-17 21:19:13 +0200

    More Cleanup

--- jpa-basic/src/main/java/no/group7/Main.java
+++ jpa-basic/src/main/java/no/group7/Main.java
@@ -7,7 +7,7 @@ import javax.persistence.Query;
 import java.util.List;
 
 public class Main {
-    private static final String PERSISTENCE_UNIT_NAME = "todos";
+    private static final String PERSISTENCE_UNIT_NAME = "tables";
     private static EntityManagerFactory factory;
 
     public static void main(String[] args) {
