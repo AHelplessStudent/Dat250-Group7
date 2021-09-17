ddcbef5bc7c09139042c263aef815fc7f4e1b2fb
AHelplessStudent <Johan.bakoy99@gmail.com>
2021-09-17 21:31:49 +0200

    Add UserAcc and VoterAcc tables.

---
+++ jpa-basic/src/main/java/no/group7/UserAcc.java
@@ -0,0 +1,58 @@
+package no.group7;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+public class UserAcc {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    private String firstName;
+    private String lastName;
+
+    // private Collection<Poll> polls;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        // TODO also return polls variable.
+        return "UserAcc [Username=" + username + ", Password=" + password + ", FirstName=" + firstName + ", LastName=" + lastName + "]";
+    }
+}
+
