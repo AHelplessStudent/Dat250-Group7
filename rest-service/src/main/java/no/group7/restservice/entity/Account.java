cfb12004006562f2b3bead6a553de6b3a12e1ae6
larsuib <78558182+larsuib@users.noreply.github.com>
2021-10-11 13:23:59 +0200

    added id to account json

--- rest-service/src/main/java/no/group7/restservice/entity/Account.java
+++ rest-service/src/main/java/no/group7/restservice/entity/Account.java
@@ -29,6 +29,14 @@ public class Account {
         this.lastName = lastName;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getUsername() {
         return username;
     }
