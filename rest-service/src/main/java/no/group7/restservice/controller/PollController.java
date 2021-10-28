99a3eea1fb02bcea3fe726c39ce8b8aec8fbe40e
arne-sas <r0802226@student.thomasmore.be>
2021-10-28 14:05:14 +0200

    First version front-end

--- rest-service/src/main/java/no/group7/restservice/controller/PollController.java
+++ rest-service/src/main/java/no/group7/restservice/controller/PollController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/polls")
 public class PollController {
