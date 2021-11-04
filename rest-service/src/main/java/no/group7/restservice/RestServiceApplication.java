3018d8142f097a3f35e55a8c13790ba6f136acc0
larsuib <78558182+larsuib@users.noreply.github.com>
2021-11-04 11:10:39 +0100

    added persistence

--- rest-service/src/main/java/no/group7/restservice/RestServiceApplication.java
+++ rest-service/src/main/java/no/group7/restservice/RestServiceApplication.java
@@ -20,25 +20,25 @@ import java.util.Optional;
 
 @SpringBootApplication
 public class RestServiceApplication {
-	private static final Logger log = LoggerFactory.getLogger(RestServiceApplication.class);
+    private static final Logger log = LoggerFactory.getLogger(RestServiceApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
 
-	@Bean
-public CommandLineRunner demo(PollRepository pollRepository, VoteRepository voteRepository, AccountRepository accountRepository) {
-		return (args) -> {
-			Poll poll1 = new Poll("Politics Poll",LocalDateTime.now(),true);
-			pollRepository.save(new Poll("Sports Poll",LocalDateTime.now(),true));
-			pollRepository.save(new Poll("Music Poll",LocalDateTime.now(),false));
-			Vote vote1 = new Vote(10,8);
-			vote1.setPoll(poll1);
+    @Bean
+    public CommandLineRunner demo(PollRepository pollRepository, VoteRepository voteRepository, AccountRepository accountRepository) {
+        return (args) -> {
+            Poll poll1 = new Poll("Politics Poll", LocalDateTime.now(), true);
+            pollRepository.save(new Poll("Sports Poll", LocalDateTime.now(), true));
+            pollRepository.save(new Poll("Music Poll", LocalDateTime.now(), false));
+            Vote vote1 = new Vote(10, 8);
+            vote1.setPoll(poll1);
 
-			voteRepository.save(vote1);
+            voteRepository.save(vote1);
 
-			poll1.getVotes().add(vote1);
-			pollRepository.save(poll1);
+            poll1.getVotes().add(vote1);
+            pollRepository.save(poll1);
 
             Collection<Poll> polls = new ArrayList<>();
             polls.add(poll1);
@@ -46,20 +46,19 @@ public CommandLineRunner demo(PollRepository pollRepository, VoteRepository vote
             acc1.setPolls(polls);
             accountRepository.save(acc1);
 
-			// fetch all customers
-			log.info("Polls found with findAll():");
-			log.info("-------------------------------");
-			for (Poll poll : pollRepository.findAll()) {
-				log.info(poll.toString());
-			}
-			log.info("");
+            // fetch all customers
+            log.info("Polls found with findAll():");
+            log.info("-------------------------------");
+            for (Poll poll : pollRepository.findAll()) {
+                log.info(poll.toString());
+            }
+            log.info("");
 
-			Optional<Poll> poll = pollRepository.findById(1L);
-			log.info("Poll found with findById(1L):");
-			log.info("--------------------------------");
-			log.info(poll.toString());
-			log.info("");
-		};
-	}
-
+            Optional<Poll> poll = pollRepository.findById(1L);
+            log.info("Poll found with findById(1L):");
+            log.info("--------------------------------");
+            log.info(poll.toString());
+            log.info("");
+        };
+    }
 }
