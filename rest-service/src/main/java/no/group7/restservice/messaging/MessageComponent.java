d7261d655714a124eb0ba149ad8b717dfc77c97a
larsuib <78558182+larsuib@users.noreply.github.com>
2021-11-09 13:42:59 +0100

    logging number of finished polls on interval. currently too fast, b.c. debugging

--- rest-service/src/main/java/no/group7/restservice/messaging/MessageComponent.java
+++ rest-service/src/main/java/no/group7/restservice/messaging/MessageComponent.java
@@ -1,6 +1,9 @@
 package no.group7.restservice.messaging;
 
+import no.group7.restservice.entity.Poll;
 import no.group7.restservice.repository.PollRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,13 +14,23 @@ public class MessageComponent {
     @Autowired
     private PollRepository pollRepository;
 
+    private Logger logger = LoggerFactory.getLogger(MessageComponent.class);
+
     /**
      * For production, we should probably have a separate application for this.
      * It will suffice for this project.
      */
     @Scheduled(fixedDelay = 1000)
     public void publishFinishedPolls() {
-        System.out.println("Checking for finished polls...");
-        pollRepository.findById(1L);
+        int numFinishedPolls = 0;
+
+        for (Poll poll : pollRepository.findAll()) {
+            if (poll.isExpired()) {
+                numFinishedPolls++;
+            }
+        }
+
+        if (numFinishedPolls > 0)
+            logger.info(numFinishedPolls + " finished polls found => sending to RabbitMQ!");
     }
 }
