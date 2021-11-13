a2027424b5a93325cff4195e97f7d76eaee7091d
arne-sas <r0802226@student.thomasmore.be>
2021-11-13 16:14:40 +0100

    First push front-end

---
+++ front-end-vote/src/components/Searchbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row class="searchbar" justify="center" align="center">
+    <v-col cols="12" sm="9">
+      <v-text-field label="PollID" placeholder="PollID"></v-text-field>
+    </v-col>
+    <v-col cols="12" sm="3">
+      <v-btn color="primary" elevation="3">Search</v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Searchbar"
+}
+</script>
+
+<style scoped>
+.searchbar{
+}
+</style>
--- front-end-vote/src/router/index.js
+++ front-end-vote/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import CreatePoll from "@/views/CreatePoll";
 
 Vue.use(VueRouter)
 
@@ -11,12 +12,17 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/dashboard',
+    name: 'Dashboard',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Dashboard.vue')
+  },
+  {
+    path: '/create',
+    name: 'CreatePoll',
+    component: CreatePoll
   }
 ]
 
