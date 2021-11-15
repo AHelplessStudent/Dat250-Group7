46ebbea20745f6a6bacf3efd8dba3d30848e7287
arne-sas <r0802226@student.thomasmore.be>
2021-11-15 16:21:02 +0100

    Front-end update

--- front-end-vote/src/views/CreatePoll.vue
+++ front-end-vote/src/views/CreatePoll.vue
@@ -14,11 +14,17 @@
       ></v-text-field>
       <h3>Start:</h3>
       <v-row>
-        <v-col cols="12" sm="6">
+        <v-col cols="12" sm="3">
           <v-date-picker v-model="dates.startTimeDate" ></v-date-picker>
         </v-col>
-        <v-col cols="12" sm="6">
+        <v-col cols="12" sm="3">
           <v-time-picker v-model="dates.startTimeHour" format="24h" scrollable></v-time-picker>
+        </v-col>
+        <v-col cols="12" sm="3">
+          <v-date-picker v-model="dates.endTimeDate" ></v-date-picker>
+        </v-col>
+        <v-col cols="12" sm="3">
+          <v-time-picker v-model="dates.endTimeHour" format="24h" scrollable></v-time-picker>
         </v-col>
       </v-row>
       <h3>End:</h3>
@@ -33,21 +39,28 @@
       <v-switch v-model="poll.public" inset label="Make poll public"></v-switch>
       <v-btn @click="submitPoll" color="primary">Create Poll</v-btn>
     </v-form>
+    <v-btn @click="show">log</v-btn>
   </v-container>
 </template>
 
 <script>
 import axios from "axios";
-import moment from "moment";
 
 export default {
   name: "CreatePoll",
   data(){
     return{
       poll: {
+        account: {
+          accountId: this.$auth.user.sub.replace(/\D/g,'') + "L",
+          id: 0,
+          firstName: this.$auth.user.given_name,
+          lastName: this.$auth.user.family_name,
+          username: this.$auth.user.nickname
+        },
         title: '',
         endTime: '2017-01-01 20:00',
-        startTime: moment(Date.now()).format('YYYY-M-D hh:mm'),
+        startTime: '2017-01-01 20:00',
         public: true,
         num_no:0,
         num_yes: 0,
@@ -58,16 +71,18 @@ export default {
         startTimeHour: '',
         endTimeDate: '',
         endTimeHour: '',
-      }
-    }
-  },
-  computed:{
-    computeStart: function () {
-      return this.starttimedate * 2
+      },
     }
   },
   methods:{
     submitPoll(){
+      //switch up start-end time
+      this.poll.startTime = this.dates.startTimeDate + " " + this.dates.startTimeHour
+      this.poll.endTime = this.dates.endTimeDate + " " + this.dates.endTimeHour
+
+      //const request = Object.assign(this.poll, this.account)
+      console.log(this.poll)
+
       axios.post('http://localhost:8080/polls', this.poll)
           .then((res) => {
             console.log(res)
